ded5d399a3efc99a6273df3a589316502e1b7b7e
Amo_Taiki_from_Desktop <c1101081@planet.kanazawa-it.ac.jp>
2024-03-02 20:18:44 +0900

    Supabaseのセッティングを進めていく

---
+++ app/src/app/account/page.tsx
@@ -0,0 +1,12 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { Database } from "@/types/supabase";
+import AccountView from "@/component/AccountInfoView";
+
+export default async function Account(){
+    const supabase = createServerComponentClient<Database>({cookies})
+    const {data: {user}} = await supabase.auth.getUser()
+    return (
+        <></>
+    )
+}
